a73e4cef431153e26c7f542e58db669340fccc34
Wolfgang <wolfgang.loeffler@gmail.com>
2021-04-11 12:33:29 +0300

    updated for latest tt-rss

--- openten.js
+++ openten.js
@@ -1,30 +1,20 @@
 require(['dojo/_base/kernel']);
 function openUnstarNext10() {
-	try {
-		var children = $$("#headlines-frame > div[id*=RROW]");
-		var processed = 0;
-		children.each(function(child) {
-			var id = child.getAttribute("data-article-id");
-			console.log("openten: id: " + id + " processed: " + processed);
-			if (child.hasClassName("marked")) { // only for starred articles!
-				console.log("openten: id: " + id + " marked, get URL...");
-				//get the URL, open and toggle marked.
-				$$("#RROW-" + id + " > div.header > span > a.title").each(function(ahref){
-					console.log("openten: open in background tab: " + ahref);
-					res = window.open(ahref, '_blank');
-					if (res == null) {
-						alert("Could not open (all) new browser windows, please allow this host to open new windows/popups in your browser settings!");
-						throw $break;
-					}
-					Headlines.toggleMark(id);
-					processed++;
-				});
-			}
-			if (processed >= 10) throw $break;
-		});
-
-
-	} catch (e) {
-		exception_error("openten", e);
+	// get all article div's which are marked==starred
+	var children = document.querySelectorAll("#headlines-frame > div.marked[id*=RROW]");
+	var processed = 0;
+	for (let child of children) {
+		var id = child.getAttribute("data-article-id");
+		console.log("openten: id: " + id + " processed: " + processed);
+		var ahref = document.querySelector("#RROW-" + id + " > div.header > span > a.title").href
+		console.log("openten: open in background tab: " + ahref);
+		res = window.open(ahref, '_blank');
+		if (res == null) {
+			alert("Could not open (all) new browser windows, please allow this host to open new windows/popups in your browser settings!");
+			return;
+		}
+		Headlines.toggleMark(id); // toggle starred
+		processed++;
+		if (processed >= 10) break;
 	}
 }
